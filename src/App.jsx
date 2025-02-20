7b41c72a706cd61ed3e21294128fbb05ade6a94f
raam9431 <118127013+raam9431@users.noreply.github.com>
2025-02-20 23:24:55 +0530

    tailwind config

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const App = () => {
+  return (
+<main className=''>
+  <h1 className='text-5xl text-violet-300 font-bold font-circular-web font-robert-medium '>
+    Welcome to Awards Website
+  </h1>
+</main>
+    )
+}
+
+export default App
